500198cda3c1e6dfe5b6e21dca53a0e387625a6f
mincheol <homerjay@naver.com>
2022-07-13 14:51:38 +0900

    crate Project, add Textfield

---
+++ Swift_Benkey/Swift_Benkey/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Swift_Benkey
+//
+//  Created by 申民鐡 on 2022/07/13.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Swift_Benkey/Swift_Benkey/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Swift_Benkey
+//
+//  Created by 申民鐡 on 2022/07/13.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
