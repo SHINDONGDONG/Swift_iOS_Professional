4d60b927eeb09524880691d2c91499518c597656
mincheol <homerjay@naver.com>
2022-07-13 14:52:22 +0900

    abcd

--- Swift_Benkey/Swift_Benkey/Login/LoginView.swift
+++ Swift_Benkey/Swift_Benkey/Login/LoginView.swift
@@ -8,13 +8,72 @@
 import UIKit
 
 class LoginView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //MARK: - Properties
+    let usernameTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Username"
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        return textfield
+    }()
+    
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //Extension 으로 style, layout을 만들어놓는다.
+        style()
+        layout()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        //기본 CGSize지정.
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+    
+    //MARK: - Configures
 
+}
+
+extension LoginView {
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemPink
+        usernameTextField.delegate = self
+        addSubview(usernameTextField)
+        
+    }
+    func layout() {
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            usernameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: usernameTextField.trailingAnchor, multiplier: 1)
+        ])
+        
+    }
+}
+
+extension LoginView:UITextFieldDelegate {
+    
+    //텍스트필드 사용을 마치고 return하면 endEdting이 실행
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Username"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
 }
--- Swift_Benkey/Swift_Benkey/Login/LoginViewController.swift
+++ Swift_Benkey/Swift_Benkey/Login/LoginViewController.swift
@@ -9,10 +9,36 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    //MARK: - Properties
+    //만들었던 기본적인 loginview를 인스턴스화 시킨다.
+    let loginView = LoginView()
+    
+    //MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
+        style()
+        layout()
     }
 
+    //MARK: - Configures
 
+}
+
+extension LoginViewController {
+    private func style() {
+        //로그인뷰를 자유자재로 움직일 수 있도록 false해줌.
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    private func layout() {
+        view.addSubview(loginView)
+        
+        //layout 을 active시켜준다.
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+    
 }
 
